f3e80fccd694bd500d1648735b1bc1b30856e53e
Jennifer Brooks <jlbrooks5@student.fullsail.edu>
2025-11-25 10:59:25 -0500

    commit additional file for save password

--- app/src/main/java/data/Profile.kt
+++ app/src/main/java/data/Profile.kt
@@ -8,6 +8,7 @@ data class Profile(
     @PrimaryKey(autoGenerate = true) val id: Int = 1,
     val name: String,
     val email: String,
+    val password: String,
     val bio: String,
     val allergies: String = "",
     val favorites: String = ""
